3576943c7f33198694ce9b3d5a96ba7fe2bc3cef
swarup03 <swarupshah03@hotmail.com>
2024-08-10 00:14:06 +0530

    9 august

--- website_custom/static/src/views/js/search_category.js
+++ website_custom/static/src/views/js/search_category.js
@@ -17,6 +17,11 @@ publicWidget.registry.searchCategoryDirect = publicWidget.Widget.extend({
 
     _bindEvents: function () {
         this.$('.input_search_category').on('input', this._get_category_data.bind(this));
+        this.$('.o_search_button').on('click', this._search_category_action(this))
+    },
+
+    _search_category_action: function () {
+
     },
 
     _get_category_data: function () {
@@ -34,16 +39,21 @@ publicWidget.registry.searchCategoryDirect = publicWidget.Widget.extend({
 
     _renderCategoryList: function (categories) {
         const $categoryList = this.$('.category_list');
-        console.log($categoryList)
         $categoryList.empty();
-        categories.forEach(category => {
-            const $item = $('<p>').text(category.name).data('category-id', category.id).addClass('category-item p-3 fs-5 border round-4');
-            $categoryList.append($item);
 
-            // Add click event for redirection
-            $item.on('click', this._redirectToCategoryPage.bind(this));
-        });
+        if (categories.length > 0) {
+            categories.forEach(category => {
+                const $item = $('<p>').text(category.name).data('category-id', category.id).addClass('category-item p-3 fs-5 border round-4');
+                $categoryList.append($item);
+
+                $item.on('click', this._redirectToCategoryPage.bind(this));
+            });
+        } else {
+            const $noRecord = $('<p>').text('No records found').addClass('text-center text-muted p-3 fs-5');
+            $categoryList.append($noRecord);
+        }
     },
+
 
     _redirectToCategoryPage: function (event) {
         const categoryId = $(event.currentTarget).data('category-id');
